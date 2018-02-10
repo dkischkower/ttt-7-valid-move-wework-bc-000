f868e6a4528452c56dcf347bae55d1911e41f616
David Kischkower <david.kischkower@wework.com>
2018-02-10 19:36:11 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@
 def position_taken?(board,index)
   if(board[index] == "" || board[index] == " " || board[index] == nil)
     return false
-  elsif()
+  elsif(board[index] == "X" || board[index] == "O")
     return true
   else
     return false
